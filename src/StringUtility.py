3fd7915cd7bcf2f25858e30a75528c636db0af73
JiaJie Fang <candycane@JiaJies-MacBook-Air.local>
2023-02-05 08:00:12 -0500

    8am

--- src/StringUtility.py
+++ src/StringUtility.py
@@ -26,46 +26,40 @@ def decipher(string):
 			new1 += chr((ord(ch1) - length - 97) % 26 + 97)
 	return new1
 
-def get_credentials(index):
+def get_credentials(email):
 	with open("users.json", "r") as file:
 		users = json.load(file)
-		username = users["user_details"][index]["username"]
-		email = users["user_details"][index]["email"]
-		password = users["user_details"][index]["password"]
-		return [username, email, password]
+		username = users.get(cipher(email))
+		password = users.get(cipher(email))
+		return [username, password]
 
-def override_to_encrypted(index):
-	encrypted_username = cipher(get_credentials(index)[0])
-	encrypted_email = cipher(get_credentials(index)[1])
-	encrypted_password = cipher(get_credentials(index)[2])
+def insert(email, username, password):
+	encrypted_username = cipher(username)
+	encrypted_email = cipher(email)
+	encrypted_password = cipher(password)
 	with open("users.json", 'r+') as file:
-		users = json.load(file)
+		try:
+			users = json.load(file)
+		except:
+			users = {}
+		if(users.get(encrypted_email) != None):
+			print('user already exists')
+			return
 		file.seek(0)
-		users["user_details"][index].update({"username":(encrypted_username),"email" : (encrypted_email), "password" : (encrypted_password)})
+		users[encrypted_email] = {"username":encrypted_username, "password":encrypted_password}
 		json.dump(users, file, indent = 4)
-		file.truncate()
 
-def retrieve_username(email):
+
+
+def retrieve_record(email):
 	cipheredEmail = cipher(email)
 	with open('users.json') as data_file:    
 		data = json.load(data_file)
-		print(data["user_details"][0])
-	for index in data["user_details"]:
-		print(data["user_details"][index][2])
-		# if(data["user_details"][index]["email"] != cipheredEmail):
-		# 	index = index + 1
-		# else:
-		# 	decrypted_username = decipher(get_credentials(index)[0])
-		# 	return decrypted_username
-
-def retrieve_password(index):
-	decrypted_password = decipher(get_credentials(index)[2])
-	return decrypted_password
+		return data.get(cipheredEmail)
 
 def main():
-	# override_to_encrypted(0)
-	# override_to_encrypted(1)
-	retrieve_username("s12@gmail.com")
+	insert("s123@gmail.com", "sharan", "pass")
+	insert("ab23@gmail.com", "andy", "pass2")
 main()
 	
 		
